dce95cef6cebd26f4a6d0778486fced968817325
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2021-04-30 22:20:22 +0200

    status working. docs done

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,21 @@
 
 All notable changes to this project will be documented in this file, in reverse chronological order by release.
 
-## 1.1.0 - 2021-04-30
+## 1.1.1 - 2021-04-30
+
+### Added
+- StatusMessage API /status
+
+### Fixed
+- nothing
+
+### Changed
+- nothing
+
+### Removed
+- nothing
+
+## 1.1.0 - 2021-04-29
 
 ### Added
 - Status /ping
--- module/Status/src/V1/Rpc/Ping/PingController.php
+++ module/Status/src/V1/Rpc/Ping/PingController.php
@@ -1,18 +1,4 @@
 <?php
-/**
- * PingController.php - Ping Controller
- *
- * Main Controller for Status Ping
- *
- * @category Controller
- * @package Status
- * @author Praesidiarius
- * @copyright (C) 2021 Praesidiarius <admin@1plc.ch>
- * @license https://opensource.org/licenses/BSD-3-Clause
- * @version 1.1.0
- * @since 1.1.0
- */
-
 namespace Status\V1\Rpc\Ping;
 
 use Laminas\Mvc\Controller\AbstractActionController;
@@ -20,12 +6,6 @@ use Laminas\ApiTools\ContentNegotiation\ViewModel;
 
 class PingController extends AbstractActionController
 {
-    /**
-     * Check if API is up and running
-     *
-     * @return ViewModel
-     * @since 1.1.0
-     */
     public function pingAction()
     {
         return new ViewModel([
