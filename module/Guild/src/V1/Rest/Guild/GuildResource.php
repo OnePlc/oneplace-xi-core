0fceae855296a10cb2012f4f57fa4e0b48d8d2ae
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2021-05-04 23:17:41 +0200

    guild create

--- module/Guild/src/V1/Rest/Guild/GuildResource.php
+++ module/Guild/src/V1/Rest/Guild/GuildResource.php
@@ -14,6 +14,7 @@
  */
 namespace Guild\V1\Rest\Guild;
 
+use Application\Controller\IndexController;
 use Laminas\ApiTools\ApiProblem\ApiProblem;
 use Laminas\ApiTools\Rest\AbstractResourceListener;
 use Laminas\ApiTools\ContentNegotiation\ViewModel;
@@ -61,6 +62,14 @@ class GuildResource extends AbstractResourceListener
     protected $mUserTbl;
 
     /**
+     * Transaction Helper
+     *
+     * @var TransactionHelper $mTransaction
+     * @since 1.0.0
+     */
+    protected $mTransaction;
+
+    /**
      * Constructor
      *
      * AchievementResource constructor.
@@ -74,6 +83,7 @@ class GuildResource extends AbstractResourceListener
         $this->mGuildUserTbl = new TableGateway('faucet_guild_user', $mapper);
         $this->mUserTbl = new TableGateway('user', $mapper);
         $this->mSession = new Container('webauth');
+        $this->mTransaction = new TransactionHelper($mapper);
     }
 
     /**
@@ -85,7 +95,75 @@ class GuildResource extends AbstractResourceListener
      */
     public function create($data)
     {
-        return new ApiProblem(405, 'The POST method has not been defined');
+        # Check if user is logged in
+        if(!isset($this->mSession->auth)) {
+            return new ApiProblem(401, 'Not logged in');
+        }
+        $me = $this->mSession->auth;
+
+        # check if user already has joined or created a guild
+        $checkWh = new Where();
+        $checkWh->equalTo('user_idfs', $me->User_ID);
+        $checkWh->notLike('date_joined', '0000-00-00 00:00:00');
+        $userHasGuild = $this->mGuildUserTbl->select($checkWh);
+
+        if(count($userHasGuild) == 0) {
+            # check if user has enough funds to create a guild
+            $guildPrice = 1000;
+            if($this->mTransaction->checkUserBalance($guildPrice,$me->User_ID)) {
+                # create guild
+                $guildName = $data->name;
+                $guildIcon = $data->icon;
+                $guildData = [
+                    'label' => $guildName,
+                    'owner_idfs' => $me->User_ID,
+                    'created_date' => date('Y-m-d H:i:s', time()),
+                    'xp_level' => 1,
+                    'xp_current' => 0,
+                    'xp_total' => 0,
+                    'icon' => $guildIcon,
+                    'is_vip' => 0,
+                    'token_balance' => 0,
+                ];
+                if($this->mGuildTbl->insert($guildData)) {
+                    # get id of new guild
+                    $newGuildId = $this->mGuildTbl->lastInsertValue;
+
+                    # join the new guild as guildmaster
+                    $this->mGuildUserTbl->insert([
+                        'guild_idfs' => $newGuildId,
+                        'user_idfs' => $me->User_ID,
+                        'rank' => 0,
+                        'date_requested' => date('Y-m-d H:i:s', time()),
+                        'date_joined' => date('Y-m-d H:i:s', time()),
+                        'date_declined' => '0000-00-00 00:00:00',
+                    ]);
+
+                    # deduct cost from user balance
+                    $newBalance = $this->mTransaction->executeTransaction($guildPrice, 1, $me->User_ID, $newGuildId, 'create-guild', 'Guild '.$guildName.' created');
+                    if($newBalance) {
+                        $guildInfo = (object)$guildData;
+                        unset($guildInfo->owner_idfs);
+                        $guildInfo->owner = (object)['id' => $me->User_ID, 'name' => $me->username,'token_balance' => $newBalance];
+                        return $guildInfo;
+                    } else {
+                        return new ApiProblem(409, 'There was an error in your coin transaction. Please contact admin');
+                    }
+                } else {
+                    return new ApiProblem(500, 'There was an error while creating the guild (db error). Please contact admin');
+                }
+            }
+        } else {
+            # load existing guild info
+            $guild = $this->mGuildTbl->select(['Guild_ID' => $userHasGuild->current()->guild_idfs]);
+            # make sure guild does still exist
+            if(count($guild) > 0) {
+                $guild = $guild->current();
+                return new ApiProblem(409, 'User is already member of the guild '.$guild->label.'. Please leave guild before creating one');
+            } else {
+                return new ApiProblem(409, 'User is already member of a removed guild. please contact admin.');
+            }
+        }
     }
 
     /**
